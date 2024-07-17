d569f577be126112a32894086970fbc324a62691
Rodrigo <rodrigomendoza721@gmail.com>
2024-07-16 21:07:21 -0300

    menu

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using EspacioDistribuidora;
+using System.Collections.Generic;
 
-
+List<Tarea> TareasPendientes = new List<Tarea>();
+List<Tarea> TareasRealizadas = new List<Tarea>();
+Console.WriteLine("\nMenu\n1. Ingresar una tarea\n2. Marcar tarea como realizada\n3. Mostrar tareas pendientes\n4. Mostrar tareas realizadas");
+switch ()
+{
+    
+    default:
+}
