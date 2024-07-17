b67292f4ac0c85db185018ead5fd238016a8ccc1
Rodrigo <rodrigomendoza721@gmail.com>
2024-07-17 12:21:01 -0300

    agregar tarea

--- Program.cs
+++ Program.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 
 List<Tarea> TareasPendientes = new List<Tarea>();
 List<Tarea> TareasRealizadas = new List<Tarea>();
-Console.WriteLine("\nMenu\n1. Ingresar una tarea\n2. Marcar tarea como realizada\n3. Mostrar tareas pendientes\n4. Mostrar tareas realizadas");
-switch ()
+Random random = new Random();
+
+//MENU
+Console.WriteLine("\nMenu\n1. Ingresar una tarea\n2. Marcar tarea como realizada\n3. Mostrar tareas pendientes\n4. Mostrar tareas realizadas\n5. Salir\n\nIngrese una opcion: ");
+string opcion = Console.ReadLine();
+int opc;
+int.TryParse(opcion, out opc);
+
+//Inicializar el id para las tareas pendientes
+int indicePendientes = 0;
+
+
+switch (opc)
 {
-    
-    default:
+    case 1: 
+    indicePendientes ++;
+    Console.WriteLine("\nIngrese la descripcion de la tarea: ");
+    string descripcion = Console.ReadLine();
+    Tarea tarea = new Tarea(indicePendientes, descripcion, random.Next(10, 100));
+    TareasPendientes.Add(tarea);
+    break; 
 }
